16c635a25ea99abc0a9a0e45d93096daeba1bb10
ntc729 <ntcminecraft1@gmail.com>
2022-06-20 14:06:41 +0100

    Added changing presence

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,18 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-        client.user.setPresence({ activities: [{ name: `/help`, type: "LISTENING" }], status: 'online' });
+        var command = '/help';
+		var action = 'LISTENING';
+		setInterval(updatePresence, 150000);
+        
+		function updatePresence(){
+			switch(command){
+				case '/help': command="/hug", action="PLAYING"; break;
+				case '/hug': command="/suggest", action="WATCHING"; break;
+				case '/suggest': command="/coinflip", action="PLAYING"; break;
+				case '/coinflip': command="/help", action="LISTENING"; break;
+			}
+			client.user.setPresence({ activities: [{ name: `${command}`, type: `${action}` }], status: 'online' })
+		}
 	},
 };
